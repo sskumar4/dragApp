c4ec2f72df8f625f5f55e6984d23c754e76bd697
Sudha SK <sskumar4@ncsu.edu>
2021-03-28 20:35:28 -0400

    added new line and associated resize, drag feature

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# dragApp
+## Project Name: DragApp
+
+## Table of Contents
+
+<!-- vscode-markdown-toc -->
+* 1. [Link repository](#Linktorepository)
+* 2. [Introduction](#Introduction)
+* 3. [Technologies](#Technologies)
+* 4. [Files](#Files)
+* 5. [Launch](#Launch)
+
+##  1. <a name='Linktorepository'></a>Link to repository
+
+* [Repository](https://github.com/sskumar4/dragApp)
+* [Link to App](https://sskumar4.github.io/dragApp/)
+
+##  2. <a name='Introduction'></a>Introduction
+An app to move and resize shapes inside the canvas. Users can connect two shapes using the lines.
+
+##  3. <a name='Technologies'></a>Technologies 
+JavaScript, HTML 5, CSS, BootStrap
+
+##  4. <a name='Files'></a>Files
+Created the following 
+js files:
+  * script.js
+Other files:
+* style.css, index.html
+
+## 6. <a name='Launch'></a>Launch
+
+* https://sskumar4.github.io/dragApp/
--- script.js
+++ script.js
@@ -12,67 +12,76 @@ var width = window.innerWidth;
       var layerRect3 = new Konva.Layer();
       var layerCircle = new Konva.Layer();
       var layerLine = new Konva.Layer();
-
+      var layerLine1 = new Konva.Layer();
       var rect1 = new Konva.Rect({
-        x: 20,
-        y: 20,
-        width: 100,
+        x: 50,
+        y: 150,
+        width: 150,
         height: 50,
-        // fill: 'green',
         stroke: 'black',
         strokeWidth: 4,
         draggable: true,
       });
-      // add the shape to the layer
+      // add rectangle 1 to the layer
       layerRect1.add(rect1);
 
       var rect2 = new Konva.Rect({
-        x: 150,
+        x: 50,
         y: 40,
         width: 100,
         height: 50,
-        // fill: 'red',
-        // shadowBlur: 10,
         stroke: 'black',
         cornerRadius: 10,
         draggable: true,
       });
+      // add rectangle 2 to the layer
       layerRect2.add(rect2);
 
       var rect3 = new Konva.Rect({
         x: 50,
-        y: 120,
+        y: 250,
         width: 100,
         height: 100,
-        // fill: 'blue',
         stroke: 'black',
         cornerRadius: [0, 10, 20, 30],
         draggable: true,
       });
+      // add rectangle 2 to the layer
       layerRect3.add(rect3);
       var circle = new Konva.Circle({
-        x: stage.width() / 4,
+        x: stage.width() / 6,
         y: stage.height() / 2,
-        radius: 70,
-        // fill: 'red',
+        radius: 50,
         stroke: 'black',
         strokeWidth: 4,
         draggable: true,
       });
 
-      // add the shape to the layer
+      // add circle to the layer
       layerCircle.add(circle);
 
       var blackLine = new Konva.Line({
-        points: [100, 370,  300, 120],
+        points: [220, 200,  300, 120],
         stroke: 'black',
-        strokeWidth: 5,
+        strokeWidth: 3,
         lineCap: 'round',
         lineJoin: 'round',
         draggable: true,
       });
-
+      // add line to the layer
       layerLine.add(blackLine);
+
+      var blackLine1 = new Konva.Line({
+        points: [300, 280,  380, 120],
+        stroke: 'black',
+        strokeWidth: 3,
+        lineCap: 'round',
+        lineJoin: 'round',
+        draggable: true,
+      });
+      // add line 1 to the layer
+      layerLine1.add(blackLine1);
+
       var trRect1 = new Konva.Transformer();
       var trRect2 = new Konva.Transformer();
       var trRect3 = new Konva.Transformer();
@@ -83,28 +92,34 @@ var width = window.innerWidth;
         rotationSnaps: [0, 90, 180, 270],
         resizeEnabled: true,
       });
+      var trLine1 = new Konva.Transformer({
+        nodes: [blackLine1],
+        centeredScaling: true,
+        rotationSnaps: [0, 90, 180, 270],
+        resizeEnabled: true,
+      });
       layerRect1.add(trRect1);
       layerRect2.add(trRect2);
       layerRect3.add(trRect3);
       layerCircle.add(trCircle);
       layerLine.add(trLine);
-
-      // by default select all shapes
-      // tr.nodes([rect1,rect2,rect3,
-      // circle, blackLine]);
+      layerLine.add(trLine1);
       trRect1.nodes([rect1]);
       trRect2.nodes([rect2]);
       trRect3.nodes([rect3]);
       trCircle.nodes([circle]);
       trLine.nodes([blackLine]);
+      trLine1.nodes([blackLine1]);
       layerRect1.draw();
       layerRect2.draw();
       layerRect3.draw();
       layerCircle.draw();
       layerLine.draw();
-      // add the layer to the stage
+      layerLine1.draw();
+      // add the shapes to the stage
       stage.add(layerRect1);
       stage.add(layerRect2);
       stage.add(layerRect3);
       stage.add(layerCircle);
       stage.add(layerLine);
+      stage.add(layerLine1);
